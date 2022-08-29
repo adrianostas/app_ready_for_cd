0573ef6cc116df744c47a2f4c7826c1736758e81
adrianostas <adrianostas87@gmail.com>
2022-08-29 10:44:13 +0300

    feat: test script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
 sudo chmod +x ./scripts/bump_version.sh
