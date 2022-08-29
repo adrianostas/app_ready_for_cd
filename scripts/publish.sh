9af435e9457cdd75f0f3bc692ab8c54ab71d8c8e
adrianostas <adrianostas87@gmail.com>
2022-08-29 13:18:46 +0300

    feat: update sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,4 @@ set -e
 
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-sudo chmod --x ./scripts/publish.sh
+sudo chmod u+x publish.sh
