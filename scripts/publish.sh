ad19009e9f8b1ad90261f3aff66aa3d8a4bb713d
adrianostas <adrianostas87@gmail.com>
2022-08-29 17:03:09 +0300

    feat: replace script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -e
 
 #sed -i '' "s/version.*/version: $1/g" pubspec.yaml
 
-
-sed -i 's/version: */version: $1/g' pubspec.yaml
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.${version}.$5.($6+1)/e' pubspec.yaml
+git add pubspec.yaml
 
 chmod +x ./scripts/publish.sh
