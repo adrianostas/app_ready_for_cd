3988151f7eebf423366c2d709ea004a2072bf388
adrianostas <adrianostas87@gmail.com>
2022-08-29 13:41:12 +0300

    feat: improve git permissions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,6 @@ set -e
 
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-git update-index --chmod u+x publish.sh
+chmod -x ./scripts/publish.sh
+git update-index --chmod=+x ./scripts/publish.sh
+chmod +x ./scripts/publish.sh
