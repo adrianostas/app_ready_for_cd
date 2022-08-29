258a8b7df138b36f05024a88dd00bd5870a4e437
adrianostas <adrianostas87@gmail.com>
2022-08-29 10:29:37 +0300

    feat: add sudo before chmod

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-chmod +x bump_version.sh
+sudo chmod +x bump_version.sh
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
