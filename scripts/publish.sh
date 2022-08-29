828f15713e151b07b8bd95ff84c7aa950e386e1f
adrianostas <adrianostas87@gmail.com>
2022-08-29 18:55:34 +0300

    feat: update sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-#sed -i '' "s/version.*/version: $1/g" pubspec.yaml
-
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.${version}.$5.($6+1)/e' pubspec.yaml
+sed -i "s/version: */version: $1/g" pubspec.yaml
 git add pubspec.yaml
 git commit -m "commit_message"
 git push origin master
