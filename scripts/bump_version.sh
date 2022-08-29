07d0e6cd956d4a93d1cd6306c9d22e8754f981db
adrianostas <adrianostas87@gmail.com>
2022-08-29 10:34:26 +0300

    feat: fix path

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,4 +3,4 @@ set -e
 
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-sudo chmod +x scripts/bump_version.sh
+sudo chmod +x ./scripts/bump_version.sh
