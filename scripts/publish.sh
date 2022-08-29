62f251770996fed6d894e9a01193e3c80f77a4dd
adrianostas <adrianostas87@gmail.com>
2022-08-29 15:44:45 +0300

    feat: add old script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-TAG=`git describe --tags`
-export TAG
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{TAG}.$5.($6+1)/e' pubspec.yaml
+sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
 git add pubspec.yaml
 chmod +x ./scripts/publish.sh
