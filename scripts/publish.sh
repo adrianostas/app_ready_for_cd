a1cf73dbc7709ac9a6e6505402d1db96851fb149
adrianostas <adrianostas87@gmail.com>
2022-08-29 15:59:43 +0300

    feat: test path

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-#git add pubspec.yaml
+sed -i '' "s/version.*/version: $1/g" pubspec.yaml
+git add pubspec.yaml
 chmod +x ./scripts/publish.sh
