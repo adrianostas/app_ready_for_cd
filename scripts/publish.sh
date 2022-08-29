dcd4d56365519f77414f92547d764b4d706ba7fb
adrianostas <adrianostas87@gmail.com>
2022-08-29 12:48:41 +0300

    feat: fix permissions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-ls -l publish.sh
+
 # Replace version of pubspec.yaml with first parameter of shell script
 sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-sudo chmod u+x ./scripts/publish.sh
+sudo chmod --x ./scripts/publish.sh
