bf9f957db65c4ee4d7315798e8fd9890fd22f1c9
adrianostas <adrianostas87@gmail.com>
2022-08-29 18:57:55 +0300

    feat: fix commit message

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -e
 
 sed -i "s/version: */version: $1/g" pubspec.yaml
 git add pubspec.yaml
-git commit -m "commit_message"
+git commit -m "update_pubspec"
 git push origin master
 
 chmod +x ./scripts/publish.sh
