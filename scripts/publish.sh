67204cf4a47743f2ebb14e027bd88ccdfe22e643
adrianostas <adrianostas87@gmail.com>
2022-08-29 13:58:04 +0300

    feat: add new publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-set -e
-
-# Replace version of pubspec.yaml with first parameter of shell script
-sed -i '' "s/^version.*/version: $1/g" pubspec.yaml
-chmod -x ./scripts/publish.sh
-git update-index --chmod=+x ./scripts/publish.sh
+#!/bin/sh
+TAG=`git describe --tags`
+export TAG
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{TAG}.$5.($6+1)/e' pubspec.yaml
+git add pubspec.yaml
 chmod +x ./scripts/publish.sh
