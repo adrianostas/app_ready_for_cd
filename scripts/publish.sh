9c66fc8eb9e380363e6847676dad2308d2beb2cd
adrianostas <adrianostas87@gmail.com>
2022-08-30 09:46:18 +0300

    feat: test increment

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 set -e
 echo "$1"
 
-awk -F'+' '{printf("%s\t%d\n",$1,$2+1)}' "pubspec.yaml"
+awk -F'+' '("%s\t%d\n",$1,$2+1)' "pubspec.yaml"
 sed -i "s/.*version: .*[+:]/version: $1+/g" "pubspec.yaml"
 git add pubspec.yaml
 git commit -m "update_pubspec"
