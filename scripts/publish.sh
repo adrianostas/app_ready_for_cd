68ce99b62a71de9bd96334fc782551c6511a9378
adrianostas <adrianostas87@gmail.com>
2022-08-30 11:56:21 +0300

    fix: test sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -e
-echo "$1"
-
+VERSION="$1"
+export VERSION
 
 
 #sed -i "s/.*version: .*[+:]/version: $1+$newBuildNumber/g" "pubspec.yaml"
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$5.($6+1)/e' pubspec.yaml
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master
