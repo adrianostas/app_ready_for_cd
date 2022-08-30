40e2416d6aad2e9af7659fe25c36ccece6d3ff63
adrianostas <adrianostas87@gmail.com>
2022-08-30 12:03:06 +0300

    feat: test with path
    BREAKING CHANGE: test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ export VERSION
 
 
 #sed -i "s/.*version: .*[+:]/version: $1+$newBuildNumber/g" "pubspec.yaml"
-perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' pubspec.yaml
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "pubspec.yaml"
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master
