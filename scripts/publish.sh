85d605b95978396a052553f0cf4cb5a15845c487
adrianostas <adrianostas87@gmail.com>
2022-08-30 11:02:04 +0300

    feat: test new script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,14 +3,10 @@
 set -e
 echo "$1"
 
-oldBuildNumber=`cut -d '+' -f2 pubspec.yaml`
 
-newBuildNumber=`expr $oldBuildNumber + 1`
 
-echo oldBuildNumber
-echo HERE newBuildNumber
-
-sed -i "s/.*version: .*[+:]/version: $1+$newBuildNumber/g" "pubspec.yaml"
+#sed -i "s/.*version: .*[+:]/version: $1+$newBuildNumber/g" "pubspec.yaml"
+perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$1.$5.($6+1)/e' pubspec.yaml
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master
