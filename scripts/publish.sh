3826e9f6e3b965101d311050a0fa889afee46713
adrianostas <adrianostas87@gmail.com>
2022-08-30 12:50:55 +0300

    feat: add back git push

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ export VERSION
 
 perl -i -pe 's/^(version:)(\s*)((\d+\.)+\d+)(\+)(\d+)$/$1.$2.$ENV{VERSION}.$5.($6+1)/e' "pubspec.yaml"
 git add pubspec.yaml
-#git commit -m "update_pubspec"
-#git push origin master
+git commit -m "update_pubspec"
+git push origin master
 
 chmod +x ./scripts/publish.sh
