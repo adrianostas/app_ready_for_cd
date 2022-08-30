aaed90a7bdca29955a38dd0054db3e120c521f63
adrianostas <adrianostas87@gmail.com>
2022-08-30 09:39:44 +0300

    fix: test script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,8 @@
 set -e
 echo "$1"
 
-sed -i "s/.*version: .*[+:]/version: $1/g" "pubspec.yaml"
+awk -F'+' '{printf("%s\t%d\n",$1,$2+1)}' "pubspec.yaml"
+sed -i "s/.*version: .*[+:]/version: $1+/g" "pubspec.yaml"
 git add pubspec.yaml
 git commit -m "update_pubspec"
 git push origin master
